96293bf7ad3edf7257ff5b38cf77824781555716
Dylinh Bun <dylinhb@gmail.com>
2025-10-01 16:35:32 -0400

    done

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// Get the information from the two inputs
-// Use the information from the two inputs and fetch the API
+const formEl = document.getElementById("color-form")
+const colorContainerEl = document.getElementById("color-container")
+const blockOne = document.getElementById("color-one")
+const blockTwo = document.getElementById("color-two")
+const blockThree = document.getElementById("color-three")
+const blockFour = document.getElementById("color-four")
+const blockFive = document.getElementById("color-five")
+const labelOne = document.getElementById("label-one")
+const labelTwo = document.getElementById("label-two")
+const labelThree = document.getElementById("label-three")
+const labelFour = document.getElementById("label-four")
+const labelFive = document.getElementById("label-five")
 
-
-let formEl = document.getElementById("color-form")
-let colorContainerEl = document.getElementById("color-container")
-
+function copy(){
+    
+}
 
 formEl.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -16,16 +25,23 @@ formEl.addEventListener("submit", (e) => {
     async function getColors(){
         const response = await fetch(`https://www.thecolorapi.com/scheme?hex=${colorSubstring}&mode=${theme}&count=5`)
         const data = await response.json()
-        console.log(data.colors)
-    }
+        console.log(data.colors[0].hex)
 
-    function getColorElements(colorsObj){
-        let palette = colorsObj.map((color) => {
-            
-        })
+        blockOne.style.backgroundColor = data.colors[0].hex.value
+        blockTwo.style.backgroundColor = data.colors[1].hex.value
+        blockThree.style.backgroundColor = data.colors[2].hex.value
+        blockFour.style.backgroundColor = data.colors[3].hex.value
+        blockFive.style.backgroundColor = data.colors[4].hex.value
+
+        labelOne.textContent = data.colors[0].hex.value
+        labelTwo.textContent = data.colors[1].hex.value
+        labelThree.textContent = data.colors[2].hex.value
+        labelFour.textContent = data.colors[3].hex.value
+        labelFive.textContent = data.colors[4].hex.value
+
+
     }
 
     getColors()
 
-    formEl.reset()
 })
